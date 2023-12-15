5312f93612529b4a3b7607d77c0d43124a3e6856
smahipala-opexcs <[saisumanthss.97@gmail.com]>
2023-12-14 19:41:34 -0600

    publish

---
+++ src/reactwordwander.js
@@ -0,0 +1,84 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import "./reactwordwander.css";
+
+const ReactWordWander = ({
+  text,
+  fontFamily,
+  fontSize,
+  textColor,
+  backgroundColor,
+}) => {
+  useEffect(() => {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let interval = null;
+
+    const onMouseOver = (event) => {
+      let iteration = 0;
+
+      clearInterval(interval);
+
+      interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if (index < iteration) {
+              return event.target.dataset.value[index];
+            }
+
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("");
+
+        if (iteration >= event.target.dataset.value.length) {
+          clearInterval(interval);
+        }
+
+        iteration += 1 / 3;
+      }, 30);
+    };
+
+    const h1Element = document.querySelector(".react-word-wander"); // Use a class for more specificity
+    if (h1Element) {
+      h1Element.onmouseover = onMouseOver;
+    }
+
+    return () => {
+      // Cleanup the event listener when the component unmounts
+      if (h1Element) {
+        h1Element.onmouseover = null;
+      }
+    };
+  }, [text]); // Add text as a dependency to update the effect when the text prop changes
+
+  const dynamicStyle = {
+    fontFamily: fontFamily || "Space Mono",
+    fontSize: fontSize || "clamp(3rem, 10vw, 5rem)",
+    color: textColor || "#000000",
+    backgroundColor: backgroundColor || "transparent",
+    whiteSpace: "nowrap",
+    padding: "1rem", // Adjust padding as needed
+    borderRadius: "clamp(0.4rem, 0.75vw, 1rem)",
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+    zIndex: 3,
+  };
+
+  return (
+    <h1 className="react-word-wander" data-value={text} style={dynamicStyle}>
+      {text}
+    </h1>
+  );
+};
+
+ReactWordWander.propTypes = {
+  text: PropTypes.string.isRequired,
+  fontFamily: PropTypes.string,
+  fontSize: PropTypes.string,
+  textColor: PropTypes.string,
+  backgroundColor: PropTypes.string,
+};
+
+export default ReactWordWander;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/ReactWordWander.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: "commonjs react",
+    "prop-types": "commonjs prop-types",
+  },
+};
